d90553f55bd28e16928297d73e118f4028880762
Emma Davenport <emma.v.davenport@gmail.com>
2025-02-08 19:13:42 +0000

    c_buf: add circular buffer library for kb support

---
+++ src/c_buf.c
@@ -0,0 +1,77 @@
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "c_buf.h"
+
+static inline uint8_t advance_headtail_value(uint8_t value, uint8_t max) {
+	if(++value == max)
+	{
+		value = 0;
+	}
+
+	return value;
+}
+
+static void advance_head_pointer(cbuf_handle_t b) {
+	if(circular_buf_full(b))
+	{
+		b->tail = advance_headtail_value(b->tail, b->max);
+	}
+
+	b->head = advance_headtail_value(b->head, b->max);
+	b->full = (b->head == b->tail);
+}
+
+cbuf_handle_t circular_buf_init(uint8_t* buffer, uint8_t size) {
+	cbuf_handle_t cbuf = pvPortMalloc(sizeof(circular_buf_t));
+
+	cbuf->buffer = buffer;
+	cbuf->max = size;
+	circular_buf_reset(cbuf);
+
+	if(!circular_buf_empty(cbuf)) {
+		// buffer reset failed
+	}
+
+	return cbuf;
+}
+
+void circular_buf_free(cbuf_handle_t b) {
+	vPortFree(b);
+}
+
+void circular_buf_reset(cbuf_handle_t b) {
+	b->head = 0;
+	b->tail = 0;
+	b->full = 0;
+}
+
+void circular_buf_put(cbuf_handle_t b, uint8_t data) {
+	b->buffer[b->head] = data;
+
+	advance_head_pointer(b);
+}
+
+uint8_t circular_buf_get(cbuf_handle_t b, uint8_t* data) {
+	uint8_t r = 0xFF;
+
+	if(!circular_buf_empty(b))
+	{
+		*data = b->buffer[b->tail];
+		b->tail = advance_headtail_value(b->tail, b->max);
+		b->full = 0;
+
+		r = 0;
+	}
+
+	return r;
+}
+
+int8_t circular_buf_empty(cbuf_handle_t b)
+{
+	return (!circular_buf_full(b) && (b->head == b->tail));
+}
+
+int8_t circular_buf_full(cbuf_handle_t b)
+{
+	return b->full;
+}
