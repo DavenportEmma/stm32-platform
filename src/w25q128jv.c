5bb09508f8f8d5fba452415f370ef8f8f8909e94
Emma Davenport <emma.v.davenport@gmail.com>
2025-03-28 16:28:33 +0000

    w25q128jv: allow users to specify w/r address
    remove hard coded addresses for write and read functions
    
    and loads of other stuff too but I've been writing code for hours at
    this point and I just want to go to bed

--- src/w25q128jv.c
+++ src/w25q128jv.c
@@ -3,51 +3,48 @@
 #include "uart.h"
 #include "w25q128jv.h"
 
-void programPage() {
+static void compileHeader(uint8_t command, uint32_t addr, uint8_t* header) {
+    uint8_t addrB0 = addr && 0xFF;
+    uint8_t addrB1 = (addr >> 8) && 0xFF;
+    uint8_t addrB2 = (addr >> 16) && 0xFF;
+
+    header[0] = command;
+    header[1] = addrB2;
+    header[2] = addrB1;
+    header[3] = addrB0;
+}
+
+static void sendHeader(uint32_t addr, W25Q128JV_CMD c) {
+    uint8_t header[4];
+    compileHeader(c, addr, header);
+    SPI_tx_rx(SPI1, header, 4);
+}
+
+void programPage(uint32_t addr, uint8_t data[], uint8_t len) {
     writeEnable(); 
 
     CS_low(GPIOA, 4);
+    
+    sendHeader(addr, PROGRAM_PAGE);
 
-    uint8_t tx[] = {
-        0x02,   // page program
-        0x00,   // 24b address (0x000000)
-        0x00,
-        0x00,
-        0xDE,
-        0xAD,
-        0xBE,
-        0xEF
-    };
+    SPI_tx_rx(SPI1, data, len);
 
-    uint8_t rx[8];
-    SPI_tx_rx(SPI1, tx, rx, 8);
     CS_high(GPIOA, 4);
 }
 
-void SPIRead() {
+void SPIRead(uint32_t addr, uint8_t data[], uint8_t len) {
     CS_low(GPIOA, 4);
 
-    uint8_t tx[] = {
-        0x03,   // read
-        0x00,   // 24b address (0x000000)
-        0x00,
-        0x00,
-        0x00,
-        0x00,
-        0x00,
-        0x00
-    };
+    sendHeader(addr, READ_DATA);
 
-    uint8_t rx[8];
-    SPI_tx_rx(SPI1, tx, rx, 8);
-    send_uart(USART3, (char*)&rx, 8);
+    SPI_tx_rx(SPI1, data, len);
+
     CS_high(GPIOA, 4);
 }
 
 void writeEnable() {
     CS_low(GPIOA, 4);
-    uint8_t tx[] = {0x06};
-    uint8_t rx[1];
-    SPI_tx_rx(SPI1, tx, rx, 1);
+    uint8_t tx[] = {WRITE_ENABLE};
+    SPI_tx_rx(SPI1, tx, 1);
     CS_high(GPIOA, 4);
 }
