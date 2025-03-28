4816cca54634028e7e607b6b0e9737e0951c1c8c
Emma Davenport <emma.v.davenport@gmail.com>
2025-03-28 16:28:30 +0000

    c_buf: add circular buffer library for kb support

---
+++ include/c_buf.h
@@ -0,0 +1,28 @@
+#ifndef _C_BUF_H
+#define _C_BUF_H
+
+typedef struct {
+    uint8_t* buffer;
+    uint8_t head;
+    uint8_t tail;
+    uint8_t max;
+    uint8_t full;
+} circular_buf_t;
+
+typedef circular_buf_t* cbuf_handle_t;
+
+cbuf_handle_t circular_buf_init(uint8_t* b, uint8_t size);
+
+void circular_buf_free(cbuf_handle_t b);
+
+void circular_buf_reset(cbuf_handle_t b);
+
+void circular_buf_put(cbuf_handle_t b, uint8_t data);
+
+uint8_t circular_buf_get(cbuf_handle_t b, uint8_t* data);
+
+int8_t circular_buf_empty(cbuf_handle_t me);
+
+int8_t circular_buf_full(cbuf_handle_t me);
+
+#endif // _C_BUF_H
